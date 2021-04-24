3ebda2ef6fbefb0bad228f8c3cc1df7078ef4df1
Adrian Iten <adrian.iten@competec.ch>
2021-04-24 15:33:15 +0200

    Restructure project for client/server architecture
    Backend with VEnv and Flask, docker to be added, check if gpu support is
    necessary

---
+++ backend/run_web_server.py
@@ -0,0 +1,25 @@
+from flask import Flask, Response, request
+from flask_cors import CORS
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/execute_filter', methods=['POST'])
+def execute_filter():
+    request_body = json.loads(request.get_data().decode("utf-8"))
+    return Response(json.dumps({request: request_body}),
+                    mimetype="application/json")
+
+@app.route('/suggest_imagenet_class', methods=['GET'])
+def suggest_imagenet_class():
+    query_string = request.args.get("query")
+    return Response(json.dumps({"class": query_string}), mimetype="application/json")
+
+@app.route('/', methods=['GET'])
+def sample():
+    return "Hello from our api!"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
+
