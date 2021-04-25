791a320cef52ba0004a439dc534475f9a0d3ccd4
Adrian Iten <adrian.iten@competec.ch>
2021-04-25 12:48:53 +0200

    Add gensim and sample with downloading vocab initially

--- backend/class_name_suggester.py
+++ backend/class_name_suggester.py
@@ -1,13 +1,15 @@
 from typing import List
-
+import gensim.downloader as api
 
 class ClassNameSuggester:
     def __init__(self):
-        # TODO: load gensim model, order vocab by frequency
-        pass
+        # TODO: load correct gensim model, order vocab by frequency
+        # see https://radimrehurek.com/gensim/auto_examples/howtos/run_downloader_api.html
+        self.model = api.load("glove-wiki-gigaword-50")
 
     # TODO: predict nearest classes
     def predict_class(self, query: str) -> List[str]:
         if query is None or query == "":
             return []
+        print(self.model.most_similar(query))
         return ["heterotroph", "cell"]
--- backend/run_web_server.py
+++ backend/run_web_server.py
@@ -9,21 +9,24 @@ CORS(app)
 
 suggester = ClassNameSuggester()
 
+
 @app.route('/execute_filter', methods=['POST'])
 def execute_filter():
     request_body = json.loads(request.get_data().decode("utf-8"))
     return Response(json.dumps({request: request_body}),
                     mimetype="application/json")
 
+
 @app.route('/suggest_imagenet_class', methods=['GET'])
 def suggest_imagenet_class():
     query_string = request.args.get("query")
     return Response(json.dumps({"classes": suggester.predict_class(query_string)}), mimetype="application/json")
 
+
 @app.route('/', methods=['GET'])
 def sample():
     return "Hello from our api!"
 
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port='5000')
-
