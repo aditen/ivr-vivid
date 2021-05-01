9cc442f06716886593ee17862d9b1ff384fe53d3
Adrian Iten <adrian.iten@competec.ch>
2021-05-01 12:15:32 +0200

    Materializing changes to input UI and add API status to app bar

--- gui/pages/index.tsx
+++ gui/pages/index.tsx
@@ -1,7 +1,8 @@
 import {NextPage} from "next";
 import {
     AppBar,
-    Button, Chip,
+    Button, Card, CardActions, CardContent,
+    Chip,
     Dialog,
     DialogContent,
     Grid,
@@ -15,10 +16,10 @@ import {
     useMediaQuery
 } from "@material-ui/core";
 import * as React from "react";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 import Head from "next/dist/next-server/lib/head";
 import {Rnd} from "react-rnd";
-import {Autocomplete, FilterOptionsState} from "@material-ui/lab";
+import {Autocomplete} from "@material-ui/lab";
 import {YoloClassImages, YoloClassName, YoloTypesAsArray} from "../src/YoloClassName";
 import {LocatedObject} from "../src/LocatedObject";
 import axios from "axios";
@@ -32,6 +33,20 @@ const MainPage: NextPage = () => {
     const [selectedClasses, setSelectedClasses] = useState<string[]>([]);
     const [searchText, setSearchText] = useState<string>("");
     const [resultMatrix, setResultMatrix] = useState<string[][]>([[]]);
+    const [apiStatus, setApiStatus] = useState<'loading' | 'error' | 'online'>("loading");
+
+    const checkApiStatus = async () => {
+        try {
+            await axios.get<any>("http://localhost:5000/");
+            setApiStatus("online");
+        } catch (e) {
+            setApiStatus("error");
+        }
+    };
+
+    useEffect(() => {
+        checkApiStatus().catch(console.error);
+    }, []);
 
     const fetchClassSuggestions = async (query: string) => {
         try {
@@ -73,117 +88,130 @@ const MainPage: NextPage = () => {
                 <img
                     src={"https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Universit%C3%A4t_Z%C3%BCrich_logo.svg/2000px-Universit%C3%A4t_Z%C3%BCrich_logo.svg.png"}
                     style={{height: 50, width: "auto", marginRight: 10}}/>
-                <Typography variant={"h4"}>
+                <Typography variant={"h4"} style={{flexGrow: 1}}>
                     ViViD</Typography>
+                {apiStatus === "online" && <Icon style={{color: "green", marginRight: 5}}>check</Icon>}
+                {apiStatus === "error" && <Icon style={{color: "red", marginRight: 5}}>clear</Icon>}
             </Toolbar>
         </AppBar>
         <Grid justify={"space-evenly"} container={true}>
             <Grid item={true} component={"div"} style={{padding: 5}}>
-                <div style={{
-                    width: isLargeScreen ? '640px' : '344px',
-                    height: isLargeScreen ? '360px' : '171px',
-                    borderColor: "black",
-                    borderWidth: "medium",
-                    borderStyle: "solid"
-                }}>
-                    {figures.map((fig, idx) => <Rnd key={idx} bounds={"parent"}
-                                                    default={{
-                                                        x: fig.xOffset,
-                                                        y: fig.yOffset,
-                                                        width: fig.width,
-                                                        height: fig.height
-                                                    }}
-                                                    onDragStop={(e, d) => {
-                                                        fig.xOffset = d.x;
-                                                        fig.yOffset = d.y;
-                                                    }}
-                                                    onResizeStop={(e, direction, ref, delta, position) => {
-                                                        fig.width = Number.parseFloat(ref.style.width.replace("px", ""));
-                                                        fig.height = Number.parseFloat(ref.style.height.replace("px", ""));
-                                                    }}
-                                                    style={{
-                                                        borderColor: "black",
-                                                        borderWidth: "medium",
-                                                        borderStyle: "dotted"
-                                                    }}>
-                        <img draggable={false} style={{width: "100%", height: "100%"}}
-                             src={!!YoloClassImages[fig.className] ? YoloClassImages[fig.className] : "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/200px-Question_mark_%28black%29.svg.png"}/>
-                    </Rnd>)}
+                <Card>
+                        <div style={{
+                            width: isLargeScreen ? '640px' : '344px',
+                            height: isLargeScreen ? '360px' : '171px',
+                            borderColor: "black",
+                            borderWidth: "medium",
+                            borderStyle: "solid"
+                        }}>
+                            {figures.map((fig, idx) => <Rnd key={idx} bounds={"parent"}
+                                                            default={{
+                                                                x: fig.xOffset,
+                                                                y: fig.yOffset,
+                                                                width: fig.width,
+                                                                height: fig.height
+                                                            }}
+                                                            onDragStop={(e, d) => {
+                                                                fig.xOffset = d.x;
+                                                                fig.yOffset = d.y;
+                                                            }}
+                                                            onResizeStop={(e, direction, ref, delta, position) => {
+                                                                fig.width = Number.parseFloat(ref.style.width.replace("px", ""));
+                                                                fig.height = Number.parseFloat(ref.style.height.replace("px", ""));
+                                                            }}
+                                                            style={{
+                                                                borderColor: "black",
+                                                                borderWidth: "medium",
+                                                                borderStyle: "dotted"
+                                                            }}>
+                                <img draggable={false} style={{width: "100%", height: "100%"}}
+                                     src={!!YoloClassImages[fig.className] ? YoloClassImages[fig.className] : "https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/200px-Question_mark_%28black%29.svg.png"}/>
+                            </Rnd>)}
 
-                </div>
+                        </div>
+                </Card>
             </Grid>
             <Grid item={true} component={"div"} xs={12} md={4} style={{padding: 5}}>
-                <div style={{display: "flex"}}>
-                    <Autocomplete
-                        onChange={(event, newValue) => {
-                            setTypeToAdd(newValue as YoloClassName);
-                        }}
-                        options={YoloTypesAsArray}
-                        fullWidth={true}
-                        renderInput={(params) => <TextField {...params} label="Choose object..." variant="outlined"/>}
-                    />
-                    <IconButton disabled={!typeToAdd} style={{marginLeft: 10}} onClick={() => {
-                        setFigures([...figures, {
-                            className: typeToAdd,
-                            height: 100,
-                            width: 100,
-                            xOffset: isLargeScreen ? 270 : 122,
-                            yOffset: isLargeScreen ? 130 : 35
-                        }])
-                    }}><Icon>add</Icon></IconButton>
-                </div>
-                <div style={{marginTop: 15, marginBottom: 15}}>
-                    <Autocomplete
-                        onInputChange={(event, newValue) => {
-                            fetchClassSuggestions(newValue).catch(console.error);
-                        }}
-                        onChange={(event, value) => {
-                            if (!!value) {
-                                selectedClasses.push(value);
-                                setSelectedClasses([...selectedClasses]);
-                            }
-                        }}
-                        filterOptions={(options: string[]) => {
-                            return options.filter(option => selectedClasses.indexOf(option) === -1);
-                        }}
-                        options={classSuggestions}
-                        fullWidth={true}
-                        renderInput={(params) => <TextField {...params} label="Choose potential image class..."
-                                                            variant="outlined"/>}
-                    />
-                </div>
-                <div>
-                    {selectedClasses.map((cls, clsIdx) => <Chip variant={"outlined"}
-                                                                style={{margin: 5}}
-                                                                key={clsIdx}
-                                                                label={cls}
-                                                                onDelete={() => {
-                                                                    selectedClasses.splice(clsIdx, 1);
-                                                                    setSelectedClasses([...selectedClasses]);
-                                                                }}
-                    />)}
-                </div>
-                <div style={{marginTop: 15, marginBottom: 15}}>
-                    <TextField fullWidth={true} label="Enter video text" variant="outlined" value={searchText} onChange={event => setSearchText(event.target.value)}/>
-                </div>
-                <div style={{marginTop: 15, marginBottom: 15}}>
-                    <Button variant={"contained"} color={"secondary"} onClick={() => {
-                        setFigures([]);
-                        setSelectedClasses([]);
-                        setSearchText("");
-                    }}><Icon>delete</Icon>Clear Query</Button>
-                </div>
-                <div>
-                    <Button variant={"contained"} color={"primary"} disabled={figures.length == 0 && selectedClasses.length == 0 && !searchText}
-                            onClick={() => executeQuery() && setQuerySubmitted(true)}><Icon>done</Icon>Submit query</Button>
-                </div>
+                <Card>
+                    <CardContent>
+                        <Typography gutterBottom variant="h4" component="h4">
+                            Query
+                        </Typography>
+                        <div style={{display: "flex"}}>
+                            <Autocomplete
+                                onChange={(event, newValue) => {
+                                    setTypeToAdd(newValue as YoloClassName);
+                                }}
+                                options={YoloTypesAsArray}
+                                fullWidth={true}
+                                renderInput={(params) => <TextField {...params} label="Choose object..."
+                                                                    variant="outlined"/>}
+                            />
+                            <IconButton disabled={!typeToAdd} style={{marginLeft: 10}} onClick={() => {
+                                setFigures([...figures, {
+                                    className: typeToAdd,
+                                    height: 100,
+                                    width: 100,
+                                    xOffset: isLargeScreen ? 270 : 122,
+                                    yOffset: isLargeScreen ? 130 : 35
+                                }])
+                            }}><Icon>add</Icon></IconButton>
+                        </div>
+                        <div style={{marginTop: 15, marginBottom: 15}}>
+                            <Autocomplete
+                                onInputChange={(event, newValue) => {
+                                    fetchClassSuggestions(newValue).catch(console.error);
+                                }}
+                                onChange={(event, value) => {
+                                    if (!!value) {
+                                        selectedClasses.push(value);
+                                        setSelectedClasses([...selectedClasses]);
+                                    }
+                                }}
+                                filterOptions={(options: string[]) => {
+                                    return options.filter(option => selectedClasses.indexOf(option) === -1);
+                                }}
+                                options={classSuggestions}
+                                fullWidth={true}
+                                renderInput={(params) => <TextField {...params} label="Choose potential image class..."
+                                                                    variant="outlined"/>}
+                            />
+                        </div>
+                        <div>
+                            {selectedClasses.map((cls, clsIdx) => <Chip variant={"outlined"}
+                                                                        style={{margin: 5}}
+                                                                        key={clsIdx}
+                                                                        label={cls}
+                                                                        onDelete={() => {
+                                                                            selectedClasses.splice(clsIdx, 1);
+                                                                            setSelectedClasses([...selectedClasses]);
+                                                                        }}
+                            />)}
+                        </div>
+                        <div style={{marginTop: 15, marginBottom: 15}}>
+                            <TextField fullWidth={true} label="Enter video text" variant="outlined" value={searchText}
+                                       onChange={event => setSearchText(event.target.value)}/>
+                        </div>
+                        <CardActions>
+                            <Button variant={"contained"} color={"primary"}
+                                    disabled={figures.length == 0 && selectedClasses.length == 0 && !searchText}
+                                    onClick={() => executeQuery() && setQuerySubmitted(true)}><Icon>done</Icon>Submit</Button>
+                            <Button variant={"contained"} color={"secondary"} onClick={() => {
+                                setFigures([]);
+                                setSelectedClasses([]);
+                                setSearchText("");
+                            }}><Icon>delete</Icon>Clear</Button>
+                        </CardActions>
+                    </CardContent>
+                </Card>
             </Grid>
         </Grid>
         <Dialog open={querySubmitted} fullScreen={true}
                 onClose={() => setQuerySubmitted(false)}>
             <DialogContent>
                 <GridList cellHeight={"auto"} cols={!!resultMatrix.length ? resultMatrix[0].length : 0}>
-                    {resultMatrix.map(matRow => matRow.filter(item => !!item).map(item => <GridListTile key={item} cols={1}>
+                    {resultMatrix.map(matRow => matRow.filter(item => !!item).map(item => <GridListTile key={item}
+                                                                                                        cols={1}>
                         <img style={{width: "100%", height: "auto"}} src={item}/>
                     </GridListTile>))}
                 </GridList>
