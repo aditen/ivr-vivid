6497a1ec7e4199f8126ecc7ba0da1ca3ac3a492a
Adrian Iten <adrian.iten@competec.ch>
2021-05-14 14:26:47 +0200

    Limit suggestions to classes that can be returned in query

--- backend/class_name_suggester.py
+++ backend/class_name_suggester.py
@@ -4,24 +4,30 @@ import re
 from typing import List
 
 import gensim.downloader as api
-import pandas as pd
-
+import mariadb
 
 class ClassNameSuggester:
     def __init__(self):
         # see https://radimrehurek.com/gensim/auto_examples/howtos/run_downloader_api.html
         self.model = api.load("glove-wiki-gigaword-50")
-        self.nasnet = pd.read_csv(os.getenv(key="PREDICTIONS_ROOT",
-                                            default='C:/Users/41789/Documents/uni/fs21/video_retrieval/') + 'nasnet_formated.csv')
+        # SELECT DISTINCT class from nasnet_classification nc where confidence > 0.5
+        self.db_connection = mariadb.connect(user=os.getenv("db_user"), password=os.getenv("db_pw"),
+                                             database=os.getenv("db_name"), host=os.getenv("db_host"), port=3306)
+        self.db_connection.auto_reconnect = True
+        cursor = self.db_connection.cursor()
+        cursor.execute("SELECT DISTINCT class from nasnet_classification nc where confidence > 0.5")
+        sql_result = cursor.fetchall()
+        self.all_classes = [tup[0] for tup in sql_result]
+        cursor.close()
+        self.db_connection.close()
         print("Initialized class name suggester")
 
     def predict_class(self, query: str) -> List[str]:
         if query not in self.model.key_to_index:
             return []
-        nasnetclasses = self.nasnet['class'].unique()
         word_to_query = query
         distances_to_word = {}
-        for x in nasnetclasses:
+        for x in self.all_classes:
             if x in self.model.key_to_index:
                 distances_to_word[x] = self.model.similarity(word_to_query, x)
             else:
@@ -34,4 +40,4 @@ class ClassNameSuggester:
                             best_candidate = sim_candidate
         res = sorted(distances_to_word.items(), key=operator.itemgetter(1), reverse=True)[:10]
         similar = list(list(zip(*res))[0])
-        return similar #[s.replace("_", " ") for s in similar]
+        return similar
